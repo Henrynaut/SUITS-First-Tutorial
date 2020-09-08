c11bb872513807ca4f3ac3f70daec5f50b3ed62e
Neil McHenry <neilmchenry@gmail.com>
2020-09-07 22:31:48 -0500

    Spawn gumball when the "E" key is pressed

---
+++ Assets/Scripts/spawnGumball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnGumball : MonoBehaviour
+{
+
+    public GameObject gumballPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            createGumball();
+        }
+    }
+
+    void createGumball()
+    {
+        Instantiate(gumballPrefab, new Vector3(0,0,0), Quaternion.identity);
+    }
+}
