ee7d10937a419648e4dd4abcbec2473bdef7228e
Neil McHenry <neilmchenry@gmail.com>
2020-09-07 23:23:34 -0500

    Spawn 100 gumballs at a time, each after a delay of 10ms

--- Assets/Scripts/spawnGumball.cs
+++ Assets/Scripts/spawnGumball.cs
@@ -7,16 +7,18 @@ public class spawnGumball : MonoBehaviour
 
     public GameObject gumballPrefab;
     public GameObject thisGumball;
+    private int numberOfGumballs;
 
     [SerializeField]
     private Material[] _materials;
     [SerializeField]
     private Renderer _renderer;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        numberOfGumballs = 100;
     }
 
     // Update is called once per frame
@@ -25,19 +27,27 @@ public class spawnGumball : MonoBehaviour
         //When E is pressed, spawn 10 gumballs
         if(Input.GetKeyDown(KeyCode.E))
         {
-            for(int i = 0; i < 10; i++)
-            {
-                createGumball();
-            }
+            createGumball(numberOfGumballs);
         }
     }
 
-    void createGumball()
+    void createGumball(int i)
     {
         //Spawn a gumball at 0,0,0 with a default "identity quaternion" rotation
-        thisGumball = Instantiate(gumballPrefab, new Vector3(0,0,0), Quaternion.identity);
-        _renderer = thisGumball.GetComponent<Renderer>();
-        ChangeMaterial();
+        //after a delay of 10 ms, "numberOfGumballs" times
+        for(int j=0; j<i; j++)
+        {
+            if (runningCoroutine == null)
+                {
+                    runningCoroutine = ExecuteAfterTime(0.1f);
+                    StartCoroutine(runningCoroutine);
+                }
+                else
+                {
+                    coroutineQueue.Enqueue(ExecuteAfterTime(0.1f));
+                }
+        }
+
     }
 
     public void ChangeMaterial () {
@@ -46,5 +56,24 @@ public class spawnGumball : MonoBehaviour
 
     private Material SelectRandomMaterial () {
         return _materials[Random.Range(0, _materials.Length)];
+    }
+
+    IEnumerator runningCoroutine = null;
+    private Queue<IEnumerator> coroutineQueue = new Queue<IEnumerator> ();
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        thisGumball = Instantiate(gumballPrefab, new Vector3(0,0,0), Quaternion.identity);
+        _renderer = thisGumball.GetComponent<Renderer>();
+        ChangeMaterial();
+
+        runningCoroutine = null;
+        if (coroutineQueue.Count > 0)
+        {
+            runningCoroutine = coroutineQueue.Dequeue();
+            StartCoroutine(runningCoroutine);
+        }
     }
 }
